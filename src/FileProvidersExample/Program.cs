b8312dda5739d8889e7e563e2d8dbb67bd72efc2
Kiran Challa <kichalla@microsoft.com>
2015-07-15 16:42:19 -0700

    added paths

--- src/FileProvidersExample/Program.cs
+++ src/FileProvidersExample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.FileProviders;
 using Microsoft.Framework.Runtime;
 
 namespace FileProvidersExample
@@ -17,7 +18,26 @@ namespace FileProvidersExample
 
         public void Main(string[] args)
         {
+            var physicalFileProvider = new PhysicalFileProvider(_appEnvironment.ApplicationBasePath);
+            physicalFileProvider.GetInfo(@"c:\github\kichalla\FileProvidersExample\src\FileProvidersExample\Program.cs");
+            physicalFileProvider.GetInfo("Program.cs");
+            physicalFileProvider.GetInfo("/Program.cs");
+            physicalFileProvider.GetInfo(@"\Program.cs");
+            physicalFileProvider.GetInfo("FileProvidersExample/Program.cs");
+            physicalFileProvider.GetInfo("/FileProvidersExample/Program.cs");
+            physicalFileProvider.GetInfo(@"\FileProvidersExample\Program.cs");
+        }
+    }
 
+    public static class FileProviderExtensions
+    {
+        public static void GetInfo(this IFileProvider provider, string path)
+        {
+            Console.WriteLine("Path         : {0}", path);
+            Console.WriteLine("Physical Path: {0}", provider.GetFileInfo(path)?.PhysicalPath);
+            Console.WriteLine("Exists       : {0}", provider.GetFileInfo(path)?.Exists);
+            Console.WriteLine("Name         : {0}", provider.GetFileInfo(path)?.Name);
+            Console.WriteLine();
         }
     }
 }

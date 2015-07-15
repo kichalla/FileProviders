3460f3ed501644e25f76aa7fa09f3c5bf15ae673
Kiran Challa <kichalla@microsoft.com>
2015-07-15 16:29:30 -0700

    Initial

---
+++ src/FileProvidersExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Framework.Runtime;
+
+namespace FileProvidersExample
+{
+    public class Program
+    {
+        private readonly IApplicationEnvironment _appEnvironment;
+
+        public Program(IApplicationEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public void Main(string[] args)
+        {
+
+        }
+    }
+}

6c6704fc736c1b0e56de27cfc9c0120954ae4989
Kiran Challa <kichalla@microsoft.com>
2015-09-17 12:23:45 -0700

    Updated to latest

--- src/FileProvidersExample/Program.cs
+++ src/FileProvidersExample/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.FileProviders;
-using Microsoft.Framework.Runtime;
+using Microsoft.Dnx.Runtime;
 using System.Reflection;
 using System.IO;
 
